d15b97db4cbbefe8bacb6620da38bf22c1933a76
widmanhg <gerardozero4@gmail.com>
2026-03-24 16:29:45 -0600

    sqlserver

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,14 +3,3 @@ output "storage_connection_string" {
   sensitive = true
 }
 
-output "storage_account_name" {
-  value = azurerm_storage_account.main.name
-}
-
-output "databricks_workspace_url" {
-  value = "https://${azurerm_databricks_workspace.main.workspace_url}"
-}
-
-output "sql_server_fqdn" {
-  value = azurerm_mssql_server.main.fully_qualified_domain_name
-}
