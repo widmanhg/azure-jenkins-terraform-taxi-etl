4daf25b6ffa393fcdd2d8d87e2660ef34e4aa325
widmanhg <gerardozero4@gmail.com>
2026-03-24 15:57:31 -0600

    sqlserver

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "tfstateetl01"
+    container_name       = "tfstate"
+    key                  = "taxi-etl.tfstate"
+  }
 }
 
 provider "azurerm" {
---
+++ terraform/outputs.tf
@@ -0,0 +1,20 @@
+output "storage_connection_string" {
+  value     = azurerm_storage_account.main.primary_connection_string
+  sensitive = true
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.main.name
+}
+
+output "databricks_workspace_url" {
+  value = "https://${azurerm_databricks_workspace.main.workspace_url}"
+}
+
+output "sql_server_fqdn" {
+  value = azurerm_mssql_server.main.fully_qualified_domain_name
+}
+
+output "sql_database_name" {
+  value = azurerm_mssql_database.main.name
+}
