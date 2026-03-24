ff6e6d09fd7aaed10cff1ed082703fb4d7693a42
widmanhg <gerardozero4@gmail.com>
2026-03-24 16:28:24 -0600

    sqlserver

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,7 +14,3 @@ output "databricks_workspace_url" {
 output "sql_server_fqdn" {
   value = azurerm_mssql_server.main.fully_qualified_domain_name
 }
-
-output "sql_database_name" {
-  value = azurerm_mssql_database.main.name
-}
